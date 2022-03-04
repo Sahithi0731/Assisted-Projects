bf345be7799c6ef73ad9d9a6043e860680c8678e
patha <patha@DESKTOP-AMGVC49>
2022-03-04 08:49:42 +0530

    Assisted Project 16, 17

---
+++ Exception Handling/src/assistedproject17/CheckException.java
@@ -0,0 +1,18 @@
+package assistedproject17;
+
+public class CheckException {
+	public static void main(String[] args) {
+
+
+		try {
+			
+			System.out.println("Hello");
+			Thread.sleep(2000);
+			System.out.println("Friends");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+}
+
+}
---
+++ Exception Handling/src/assistedproject17/LowBalanceException.java
@@ -0,0 +1,4 @@
+package assistedproject17;
+
+public class LowBalanceException extends Exception { // checked exp
+}
